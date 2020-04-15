5c2822f33966a55d71598e3fd26b7945483f5ab7
OliveLake <52832181+OliveLake@users.noreply.github.com>
2020-04-15 03:25:34 -0700

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -50,4 +50,4 @@ def ml_loop():
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
-            comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+            comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_RIGHT)
